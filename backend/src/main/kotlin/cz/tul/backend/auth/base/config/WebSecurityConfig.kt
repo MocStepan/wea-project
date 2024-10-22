8e1b491fd24fcb9d226a76a8a4299d0f90e23677
Stepan_Moc <stepan.moc@tul.cz>
2024-10-22 20:25:00 +0000

    Resolve "Hotfix: secret for jwt"

--- backend/src/main/kotlin/cz/tul/backend/auth/base/config/WebSecurityConfig.kt
+++ backend/src/main/kotlin/cz/tul/backend/auth/base/config/WebSecurityConfig.kt
@@ -35,7 +35,7 @@ class WebSecurityConfig(
   private val unsecuredEndpoints =
     arrayOf(
       "/api/v1/auth/login",
-      "/api/v1/auth/register",
+      "/api/v1/auth/registration",
       "/api/v1/auth/logout",
       "/api/v1/auth/invoke-refresh-token",
       "api/*/docs/**",
